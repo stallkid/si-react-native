1fdceefb8fabb4bccb90564e49c48f6a76b338ea
stallkid <renan.asthusius@hotmail.com>
2019-03-24 01:06:50 -0300

    OpenCV blurred photo example

--- android/app/src/main/java/com/sociedade_inteligente/MainApplication.java
+++ android/app/src/main/java/com/sociedade_inteligente/MainApplication.java
@@ -3,6 +3,8 @@ package com.sociedade_inteligente;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,6 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
+            new RNCameraPackage(),
               new RNOpenCvLibraryPackage()
       );
     }
